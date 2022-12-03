b4d29ad60d59d59551e66949b4904dadd88678d0
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-03 13:38:26 -0800

    task 8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str.split()
+print(str[5:7] + str[12:13] + str[0:1])
