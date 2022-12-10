6df7b1a36beb2bf383d57e1d28c5777808ffa6be
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-10 10:13:02 -0800

    task1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    a = 10
+    b = 5
+
+    if add:
+        print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+    
+    if sub:
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a,b)))
+    
+    if mul:
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a,b)))
+    
+    if div:
+        print("{:d} / {:d} = {:d}".format(a, b, div(a,b)))
+
+if __name__ == "__main__":
+    main()
+
