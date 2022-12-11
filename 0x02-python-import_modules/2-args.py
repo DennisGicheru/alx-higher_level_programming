792723b9373c1fa43b6bd2f4676eed3bbf586262
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-10 22:25:16 -0800

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+n = len(sys.argv)
+
+if n == 2:
+    print("{:d} argument:".format(n-1))
+    print("1: {}".format(sys.argv[1]))
+
+elif n <= 1:
+    print("0 arguments.")
+
+else:
+    print("{:d} arguments:".format(n-1))
+    
+    for i in range(1, n):
+        print("{:d}: {}".format(i, sys.argv[i]))
