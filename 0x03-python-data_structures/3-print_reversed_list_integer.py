d40cce2c77717c881584226c78c5711d40c845ce
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-13 02:39:41 -0800

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+
+
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        print('', end='')
+    else:
+        my_list.reverse()
     n = len(my_list)
-    for i in range(n)):
-        my_list[i] = my_list[n-i]
-        return my_list
+    for i in range(n):
+        print("{:d}".format(my_list[i]))
