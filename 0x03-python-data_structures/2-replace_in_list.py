8be7ef054f4124ed9a21cf43c20156296a0eaedf
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-13 01:16:57 -0800

    task 2-replace element edit

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,8 +4,8 @@ def replace_in_list(my_list, idx, element):
         return(my_list)
 
     elif idx >= len(my_list):
-         return(my_list)
-        
+        return(my_list)
+
     else:
         my_list[idx] = element
         return(my_list)
