00998a5f99f9eb1c9b4575eb40d3e06fa41f95d4
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-13 21:22:03 -0800

    task 5-remove characters from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@
 
 def no_c(my_string):
     if my_string is not None:
-        new_string = my_string.translate({ord(c):None for c in "cC"})
+        new_string = my_string.translate({ord(c): None for c in "cC"})
         return (new_string)
