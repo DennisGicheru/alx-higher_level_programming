efb19c55282630af7fdccab842ec9cb7ce77d6b8
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-17 10:25:07 -0800

    task 0-using lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,6 @@
 
 
 def square_matrix_simple(matrix=[]):
-    return ([[row[i] ** 2 for i in range(3)] for row in matrix])
+    #return ([[row[i] ** 2 for i in range(3)] for row in matrix]) - Also works
+    return ([list(map(lambda x: x ** 2, i)) for i in matrix]) 
+
