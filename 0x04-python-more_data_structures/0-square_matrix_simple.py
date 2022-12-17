c866fecfc0a10411d8fea0fbafb9e8aa80bb83df
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-17 09:35:11 -0800

    task 0-edit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,12 @@
 
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix
-    return ([[row[i] ** 2 for i in range(3)] for row in new_matrix])
+    new_matrix = []
+    if matrix == []:
+        return matrix
+    else:
+        new_matrix = matrix
+        return ([[row[i] ** 2 for i in range(3)] for row in new_matrix])
+    
     return (new_matrix)
     return (matrix)
