bb1c5d787097bd4dd861b3e7319e8ba43f93cf64
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-18 08:51:01 -0800

    task 4- only different elements

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def only_diff_elements(set_1, set_2):
+ #  return(set_1 ^ set_2) - short answer(works)
+    list_1 = list(set_1)
+    list_2 = list(set_2)
+    list_3 = []
+    for i in list_1:
+        if i not in list_2:
+            list_3.append(i)
+    for j in list_2:
+        if j not in list_1:
+            list_3.append(j)
+    return (list_3)
