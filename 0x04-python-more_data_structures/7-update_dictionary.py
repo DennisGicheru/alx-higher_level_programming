77fd23a3c47e117e1864a63ffecfa5490763b9f2
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-18 21:37:30 -0800

    task 7-updating dictionaries

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    for item in a_dictionary:
+        if item == key:
+            a_dictionary[key] = value
+    return(a_dictionary)
