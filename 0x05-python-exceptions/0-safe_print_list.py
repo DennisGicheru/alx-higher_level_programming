23da551f60b50edc2a1c3cbf326a0fc54bb7435f
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-20 02:30:57 -0800

    0-safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,9 +10,7 @@ def safe_print_list(my_list=[], x=0):
             try:
                 print("{}".format(my_list[i]), end="")
                 n += 1
-                if n >= x:
-                    break
-            except TypeError:
-                print("Type Error found")
+            except IndexError:
+                break
         print("")
         return(n)
