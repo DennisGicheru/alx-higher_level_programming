2ddc8e931668c12702fca4131d91c5979a6dd2e7
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-20 01:57:17 -0800

    task 0-final

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,17 +3,20 @@
 
 def safe_print_list(my_list=[], x=0):
     n = 0
-    for i in my_list:
-        try:
-            print("{}".format(my_list[i-1]), end="")
-            n += 1
-            if n >= x:
-                break
-            else:
-                continue
-        except IndexError:
-            print("error,index out of range")
-        except TypeError:
-            print("type error detected")
-    print("")
-    return(n)
+    if my_list is None:
+        return None
+    else:
+        for i in my_list:
+            try:
+                print("{}".format(my_list[i-1]), end="")
+                n += 1
+                if n >= x:
+                    break
+                else:
+                    continue
+            except IndexError:
+                print("error,index out of range")
+            except TypeError:
+                print("type error detected")
+        print("")
+        return(n)
