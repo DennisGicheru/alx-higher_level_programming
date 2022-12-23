2544c30aa97de2616eff8032162fd1ef5b7b20b5
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-23 05:44:56 -0800

    task 1-define square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class that defines a square by Private Instance Attribute
+Instantiation by size
+"""
+
+
+class Square:
+    """
+    defines a square with a size
+    """
+    def __init__(self, size):
+        self.__size = size
