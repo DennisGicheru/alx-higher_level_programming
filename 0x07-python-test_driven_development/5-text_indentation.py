c883f9d3016795c9d06bd41955593db8076f5c84
DennisGicheru <dennisgicheru698@gmail.com>
2022-12-30 04:24:49 -0800

    task 5 - text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+simple function that prints a text with 2 new lines
+after certain characters are found
+"""
+
+
+def text_indentation(text):
+    """
+    prints two new lines after certain characters
+    Args:
+        text: the text or paragraph to be examined(must be str)
+        first character .
+        second character ?
+        third character :
+    each character to be replaced with two new lines
+    Raise:
+        TypeError: when text is not string
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+ 
+    for i in text:
+        a = ord(i)
+        if a == 46 or a == 63 or a == 58:
+            print(i)
+            print()
+        else:
+            print(i, end="")
