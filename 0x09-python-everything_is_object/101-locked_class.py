a0b8c199c493f13ec2462586225cbf84cc8887cc
DennisGicheru <dennisgicheru698@gmail.com>
2023-01-06 09:19:39 -0800

    advanced tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class with no attribute or object
+"""
+
+
+class LockedClass:
+    """
+    will not create class until name is 
+    first_name
+    """
+    __slots__ = 'first_name'
