87b0d65728261e5c039727f841407feb0aecb04c
DennisGicheru <dennisgicheru698@gmail.com>
2023-01-16 01:52:37 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Class Mylist that inherits from list"""
+class MyList(list):
+    """class MyList"""
+    def print_sorted(self):
+        """public instance method"""
+        print("{}".format(sorted(self)))
