b394c34b35881dd44e5556674f27fabd0bac5c00
DennisGicheru <dennisgicheru698@gmail.com>
2023-01-20 05:36:51 -0800

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+
+"""Returns the JSON representation
+   of an object(string)"""
+
+
+def to_json_string(my_obj):
+    """use dumps method"""
+    return (json.dumps(my_obj))
