6221946a305dd8bb795774851a40855c39bb8ecf
DennisGicheru <dennisgicheru698@gmail.com>
2023-01-20 05:13:59 -0800

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""appends string at the end of a text file"""
+
+def append_write(filename="", text=""):
+    """open file, then append
+    to file"""
+    with open(filename, "a", encoding="UTF-8") as f:
+        """write to file"""
+        f.write(text)
+        return (len(text))
