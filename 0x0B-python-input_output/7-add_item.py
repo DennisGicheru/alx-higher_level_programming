2322e2d7e81154e922570874df7ff7bd50a3983f
DennisGicheru <dennisgicheru698@gmail.com>
2023-01-21 03:15:47 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,18 +2,15 @@
 """
 Function that adds all arguments to a python list
 """
-import json
 import sys
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_to_json_file
 
-args = sys.argv
-filename = "add_item.json"
-with open(filename, 'a+', encoding="utf-8") as f:
-    """add to python list then save to file
-    use extend command for it"""
-    my_list = []
-    my_list.extend(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+open("add_item.json", "a")
+try:
+    var = load_from_json_file("add_item.json")
+except ValueError:
+    var = []
+save_to_json_file(var + sys.argv[1:], "add_item.json")
