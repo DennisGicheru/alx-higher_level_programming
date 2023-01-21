6aeee512c5bfbeb424bfc37b04c615cb2f886cd2
DennisGicheru <dennisgicheru698@gmail.com>
2023-01-21 03:18:47 -0800

    task 7-edit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,9 +8,9 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_to_json_file
 
-open("add_item.json", "a")
-try:
-    var = load_from_json_file("add_item.json")
-except ValueError:
-    var = []
-save_to_json_file(var + sys.argv[1:], "add_item.json")
+with open("add_item.json", "a") as f:
+    try:
+        var = load_from_json_file(f)
+    except ValueError:
+        var = []
+    save_to_json_file(var + sys.argv[1:], f)
