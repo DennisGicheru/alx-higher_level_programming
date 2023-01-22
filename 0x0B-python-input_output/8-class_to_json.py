ad93b481aa13e75198ba6df6ec7a7e2d1f2c0e7d
DennisGicheru <dennisgicheru698@gmail.com>
2023-01-22 00:59:30 -0800

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Returns dictionary description \ 
+with simple data structure
+"""
+
+
+def class_to_json(obj):
+    """returns dictionary description 
+    with simple data structure (list, 
+    dictionary, string integer)
+    """
+    return(obj.__dict__)
