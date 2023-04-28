0ccd14d20cb6a774b40e3bdfc1a162f7b50151cb
DennisGicheru <dennisgicheru698@gmail.com>
2023-04-28 11:16:05 -0700

    fix errors 13,14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
 -- script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-INSERT INTO second_table(average)
-	VALUES ( AVG(score));
+SELECT AVG(score) AS average FROM second_table;
