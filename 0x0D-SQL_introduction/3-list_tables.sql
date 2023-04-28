175171339d2a1f458f621a27c9b4936afc4f4df1
DennisGicheru <dennisgicheru698@gmail.com>
2023-04-28 10:01:49 -0700

    corrections

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,3 @@
 --script that lists all the tables of a database in your MySQL server.
+SELECT DATABASE mysql
 SHOW TABLES;
--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
 --lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
-SELECT * FROM first_table WHERE row LIKE "%"
+SHOW TABLES FROM hbtn_0c_0 WHERE ROW LIKE "%";
