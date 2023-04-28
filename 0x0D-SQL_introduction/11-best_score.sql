aee579484c3be53df175c1c26c295aeae44f9da0
DennisGicheru <dennisgicheru698@gmail.com>
2023-04-28 10:51:14 -0700

    fix errors 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
 SELECT second_table FROM hbtn_0c_0
-WHERE second_table.score >= 10
+WHERE score >= 10
 ORDER BY score DESC;
