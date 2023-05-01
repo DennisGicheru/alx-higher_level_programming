18ddf269be7d828964f59253f6f007464bca22c3
DennisGicheru <dennisgicheru698@gmail.com>
2023-05-01 03:38:25 -0700

    tasks 8,9 fix errors

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa
 -- Each record should display: cities.id - cities.name - states.name
 -- results should be sorted in ascending order by cities.id
-USE hbtn_0d_usa;
 SELECT cities.id,
        cities.name,
        states.name
