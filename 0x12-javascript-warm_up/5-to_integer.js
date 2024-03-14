d65da4cff0f9d6d5ce7aed65543aaa0887d3fd82
DennisGicheru <dennisgicheru698@gmail.com>
2024-03-14 20:30:31 +0300

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,8 +3,8 @@
 // convert first argument to integer and print number where possible
 // else print "Not a number"
 const x = process.argv[2];
- if (isNaN(x)) {
-  console.log("Not a number");
+if (isNaN(x)) {
+  console.log('Not a number');
 } else {
   console.log('My Number: ' + parseInt(x));
 }
