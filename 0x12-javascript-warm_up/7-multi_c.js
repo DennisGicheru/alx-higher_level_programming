d4dc0a402093e3b0860c08c58a485fe0d543ff48
DennisGicheru <dennisgicheru698@gmail.com>
2024-03-15 19:56:02 +0300

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const x = process.argv[2];
+
+if ((Number.isNaN(x)) || (x === undefined)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
