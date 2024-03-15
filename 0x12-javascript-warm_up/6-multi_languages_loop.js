5e36851d28e2c0ff86b4ac5854e2c918abb86a64
DennisGicheru <dennisgicheru698@gmail.com>
2024-03-15 06:21:59 +0300

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // prints multi lines in JavaScript
-const arrStr = ['C is fun',
+const multi = ['C is fun',
   'Python is cool',
   'JavaScript is amazing'];
-for (let i = 0; i < 3; ++i) { console.log(arrStr[i]); }
+for (let i = 0; i < multi.length; ++i) { console.log(multi[i]); }
