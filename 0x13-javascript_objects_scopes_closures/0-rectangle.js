1878ce52f591dcb7e6370bb1b2ba953202db9909
DennisGicheru <dennisgicheru698@gmail.com>
2024-03-24 23:35:54 +0300

    Readme file

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// create empty class Rectangle that defines a rectangle
+class Rectangle {
+	constructor(Rectangle = (length, width)) {
+		this.length = length;
+		this.width = width;	
+	}
+}
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,5 @@
+# Javascript Objects
+Javascript objects
+Creating variable types and scope
+Closures and Prototypes
+Object Inheritance in JavaScript
