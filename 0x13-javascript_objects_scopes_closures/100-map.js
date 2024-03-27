59f29ae02d0b1fe4dbc2456f07c02e3b4f3099db
DennisGicheru <dennisgicheru698@gmail.com>
2024-03-27 20:03:27 +0300

    advanced tasks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let list = require('./100-data.js').list;
+const list = require('./100-data.js').list;
 
 console.log(list);
-let map1 = list.map((x, index) => x * index);
+const map1 = list.map((x, index) => x * index);
 console.log(map1);
